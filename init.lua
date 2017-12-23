273eb8387e47d0898ce847d334183aa1730cce39
Techbot <techbot121@gmail.com>
2017-12-23 18:01:03 +0100

    merge command info into one line

--- init.lua
+++ init.lua
@@ -93,8 +93,7 @@ end)
 client:on("messageCreate", function(message)
 	if message.channel == channel and message.author ~= client.user then
 		if message.content:starts(".") then
-			c:say("#metastruct", "Command call requested by " .. message.author.username .. ":")
-			c:say("#metastruct", message.content)
+			c:say("#metastruct", "Command call requested by " .. message.author.username .. ":" .. message.content)
 		else
 			local hasAttachments = message.attachment
 			local attachments = ""
