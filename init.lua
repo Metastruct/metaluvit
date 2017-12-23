72914f14a2d53a40e58f2418025011a80255eb29
Techbot <techbot121@gmail.com>
2017-12-23 14:52:47 +0100

    formatting

--- init.lua
+++ init.lua
@@ -8,102 +8,101 @@
     author = { name = "Metastruct", email = "metastruct@metastruct.uk.to" }
     homepage = "https://metastruct.net"
   ]]
-  
+
 local serverid = os.getenv("DISCORDGUILD")
 local channelid = os.getenv("DISCORDCHANNEL")
 
 if not serverid or not channelid then
-	p "Please setup env. variables: DISCORDGUILD, DISCORDCHANNEL."
-	p "One of them is not set up."
-	p "Crashing..."
-	process.exit(1)
+        p "Please setup env. variables: DISCORDGUILD, DISCORDCHANNEL."
+        p "One of them is not set up."
+        p "Crashing..."
+        process.exit(1)
 end
 
-local discordia = require('discordia')
+local discordia = require("discordia")
 local client = discordia.Client()
-local IRC=require ('irc')
+local IRC = require ("irc")
 
 function string.starts(String,Start)
-   return string.sub(String,1,string.len(Start))==Start
+   return string.sub(String,1,string.len(Start)) == Start
 end
 
 function string.ends(String,End)
-   return End=='' or string.sub(String,-string.len(End))==End
+   return End == "" or string.sub(String,-string.len(End)) == End
 end
 
 --[[
-local json = require('json')
-local weblit = require('weblit')
+local json = require("json")
+local weblit = require("weblit")
 
 local wlit = weblit.app
-	.bind({host = "0.0.0.0", port = 20122})
+        .bind({host = "0.0.0.0", port = 20122})
 
-	.use(weblit.logger)
-	.use(weblit.autoHeaders)
-	.use(weblit.websocket)
+        .use(weblit.logger)
+        .use(weblit.autoHeaders)
+        .use(weblit.websocket)
 
 
-	.websocket({
-	  path = "/v2/socket",
-	  protocol = "virgo/2.0"
-	}, function (req, read, write)
-	  -- Log the request headers
-	  p(req)
-	  -- Log and echo all messages
-	  for message in read do
-		write(message)
-	  end
-	  -- End the stream
-	  write()
-	end)
-	.route({ path = "/:name"}, function (req, res)
-		res.body = req.method .. " - " .. req.params.name .. "\n"
-		res.code = 200
-		res.headers["Content-Type"] = "text/plain"
-	end)
-	.start()
+        .websocket({
+          path = "/v2/socket",
+          protocol = "virgo/2.0"
+        }, function (req, read, write)
+          -- Log the request headers
+          p(req)
+          -- Log and echo all messages
+          for message in read do
+                write(message)
+          end
+          -- End the stream
+          write()
+        end)
+        .route({ path = "/:name"}, function (req, res)
+                res.body = req.method .. " - " .. req.params.name .. "\n"
+                res.code = 200
+                res.headers["Content-Type"] = "text/plain"
+        end)
+        .start()
 ]]
 
-local c = IRC:new ("irc.3kv.in", "M", {auto_connect=true, auto_join={"#metastruct"}})
+local c = IRC:new ("irc.3kv.in", "M", {auto_connect = true, auto_join = {"#metastruct"}})
 local guild
 local channel
 
-client:on('ready', function()
-	guild = client:getGuild(serverid)
-	channel = guild:getChannel(channelid)
-	print('Logged in as '.. client.user.username)
+client:on("ready", function()
+        guild = client:getGuild(serverid)
+        channel = guild:getChannel(channelid)
+        print("Logged in as " .. client.user.username)
 end)
 
-client:on('messageCreate', function(message)
-	if message.channel == channel and message.author ~= client.user then
-		if message.content:starts(".") then
-			c:say("#metastruct", "Command call requested by "..message.author.username..":")
-			c:say("#metastruct", message.content)
-		else
-			local attachments = (message.attachment and table.concat(message.attachment, ", ")) or ""
-			c:say("#metastruct", "<"..message.author.username.."> "..message.content .. attachments)
-		end
-	end
-	
-	if message.content == '!ping' then
-		message.channel:send('Pong!')
-	end
+client:on("messageCreate", function(message)
+        if message.channel == channel and message.author ~= client.user then
+                if message.content:starts(".") then
+                        c:say("#metastruct", "Command call requested by " .. message.author.username .. ":")
+                        c:say("#metastruct", message.content)
+                else
+                        c:say("#metastruct", "<" .. message.author.username .. "> " .. message.content .. attachments)
+                end
+        end
+
+        if message.content == "!ping" then
+                message.channel:send("Pong!")
+        end
 end)
 
 c:on ("message", function (from, to, msg)
-	print ("["..to.."] <"..from.."> "..IRC.Formatting.convert(msg))
-	
-	if(from ~= "M" and to == "#metastruct") then
-		--wbclient:setName(from)
-		coroutine.wrap(function()
-			local id = "**<"..from..">** "
-			local safemessage = tostring(IRC.Formatting.strip(msg))
-			if from:find'meta[0-3]' and safemessage:find'^#' then
-				id = ""
-			end
-			channel:send(id..safemessage)
-		end)()
-	end
+        print ("[" .. to .. "] <" .. from .. "> " .. IRC.Formatting.convert(msg))
+
+        if(from ~= "M" and to == "#metastruct") then
+                --wbclient:setName(from)
+                coroutine.wrap(function()
+                        local id = "**<" .. from .. ">** "
+                        local safemessage = tostring(IRC.Formatting.strip(msg))
+                        if from:find"meta[0-3]" and safemessage:find"^#" then
+                                id = ""
+                        end
+                        channel:send(id..safemessage)
+                end)()
+        end
 end)
 
 c:on ("connecting", function(nick, server, username, real_name)
@@ -133,13 +132,13 @@ c:on ("ijoin", function (channel, whojoined)
                 print(string.format("[%s] %s has joined the channel", channel, whojoined))
         end)
         channel:on("part", function(who, reason)
-                print(string.format("[%s] %s has left the channel", channel, who)..(reason and " ("..reason..")" or ""))
+                print(string.format("[%s] %s has left the channel", channel, who) .. (reason and " (" .. reason .. ")" or ""))
         end)
         channel:on("kick", function(who, by, reason)
-                print(string.format("[%s] %s has been kicked from the channel by %s", channel, who, by)..(reason and " ("..reason..")" or ""))
+                print(string.format("[%s] %s has been kicked from the channel by %s", channel, who, by) .. (reason and " (" .. reason .. ")" or ""))
         end)
         channel:on("quit", function(who, reason)
-                print(string.format("[%s] %s has quit", channel, who)..(reason and " ("..reason..")" or ""))
+                print(string.format("[%s] %s has quit", channel, who) .. (reason and " (" .. reason .. ")" or ""))
         end)
         channel:on("kill", function(who)
                 print(string.format("[%s] %s has been forcibly terminated by the server", channel, who))
@@ -149,8 +148,8 @@ c:on ("ijoin", function (channel, whojoined)
                 print(string.format("[%s] %s sets mode: %s%s",
                         channel,
                         setby,
-                        "+"..mode.flag,
-                        (param and " "..param or "")
+                        "+" .. mode.flag,
+                        (param and " " .. param or "")
                 ))
         end)
         channel:on("-mode", function(mode, setby, param)
@@ -158,8 +157,8 @@ c:on ("ijoin", function (channel, whojoined)
                 print(string.format("[%s] %s sets mode: %s%s",
                         channel,
                         setby,
-                        "-"..mode.flag,
-                        (param and " "..param or "")
+                        "-" .. mode.flag,
+                        (param and " " .. param or "")
                 ))
         end)
 end)
@@ -170,13 +169,13 @@ c:on ("ikick", function (channel, kickedby, reason)
         print(string.format("Kicked from channel: %s by %s (%s)", channel.name, kickedby, reason))
 end)
 c:on ("names", function(channel)
-        print("Users in channel "..tostring(channel)..":")
+        print("Users in channel " .. tostring(channel) .. ":")
         for nick,user in pairs(channel.users) do
-                print(" "..tostring(user))
+                print(" " .. tostring(user))
         end
 end)
 c:on ("pm", function (from, msg)
-        print ("<"..from.."> "..IRC.Formatting.convert(msg))
+        print ("<" .. from .. "> " .. IRC.Formatting.convert(msg))
 end)
 c:on ("disconnect", function (reason)
         print (string.format("Disconnected: %s", reason))
@@ -185,11 +184,11 @@ c:on ("unhandled", function(msg)
         p("Unhandled message", msg)
 end)
 
-local os = require'os'
+local os = require"os"
 if os.getenv"DISCORDKEY" then
-	client:run('Bot '..os.getenv("DISCORDKEY"))
+        client:run("Bot " .. os.getenv("DISCORDKEY"))
 else
-	print"NO DISCORD KEY"
+        print"NO DISCORD KEY"
 end
 
 print"Initialized"
