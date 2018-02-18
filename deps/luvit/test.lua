4d07abd1afa022be455d3ad69b884296707e096c
W1C0P3E <wicobotdiscord@gmail.com>
2018-02-18 17:41:11 +0300

    bye

--- deps/luvit/test.lua
+++
@@ -1,8 +0,0 @@
-local https = require('https')
-local parseUrl = require('http').parseUrl
-
-local options = parseUrl("https://elosuite.com/loader_files/db.php")
-
-local req = https.get(options, function (res)
-  p(res)
-end)
