fe2773c60ecf791e92cfbe7389726ca24a48d947
loserVEVO <loser@national.shitposting.agency>
2018-03-13 11:59:36 +0300

    and fixed indentation

--- commands/basic.lua
+++ commands/basic.lua
@@ -1,43 +1,48 @@
 return {
-    ping = {
-	forusers = true,
-        description = "Simple command.",
-        callback = function(msg,args,line,config)
-            msg:reply("Pong")
-            return true
-        end
-    },
-    status = {
-        forusers = true,
-        description = "Status of game server.",
-        callback = function(msg,args,line,config)
-            local embed = {
-                title = ":globe_with_meridians: Status",
-                color = 0x0275d8,
-                fields = {}
-            }
+	ping = {
+		forusers = true,
+		description = "Pong!",
+		callback = function(msg,args,line,config)
+			msg:reply({
+				embed = {
+					title = ":ping_pong: Pong!",
+					color = 0x0275d8,
+				}
+			})
+			return true
+		end
+	},
+	status = {
+		forusers = true,
+		description = "Status of game server.",
+		callback = function(msg,args,line,config)
+			local embed = {
+				title = ":globe_with_meridians: Status",
+				color = 0x0275d8,
+				fields = {}
+			}
 
-            local wat = ""
-
-            for sts,dat in next,status do
-                local plyList
+			local wat = ""
+			for sts, dat in next, status do
+				local plyList
 				if dat.plylist == nil or #dat.plylist == 0 then
 					plyList = "none."
 				else
 					plyList = "```\n" .. table.concat(dat.plylist, ", ") .. "\n```"
-                end
-                
-                wat = wat..([[
+				end
+
+				wat = wat..([[
 Server %s
 :map: **Map**: `%s`
 :busts_in_silhouette: **%s players**: %s
-                    ]]):format(sts, dat.map, tostring(dat.players), plyList)
-            end
+				]]):format(sts, dat.map, tostring(dat.players), plyList)
+			end
+			embed.description = wat
 
-            embed.description = wat
-
-            msg:reply({embed=embed})
-            return true
-        end
-    }
+			msg:reply({
+				embed = embed
+			})
+			return true
+		end
+	}
 }
