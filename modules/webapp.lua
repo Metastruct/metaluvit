6a7eb577fef68ec933a9f2866a805441dcd18db5
Python1320 <python1320+gh@gmail.com>
2019-10-18 02:26:55 +0300

    :94: attempt to index field 'status' (a nil value)

--- modules/webapp.lua
+++ modules/webapp.lua
@@ -12,6 +12,7 @@ end
 
 local client = instances.discord
 local webhook = string.Split(config.webhook, "/")
+
 local function execWebhook(tbl)
 	return client and client._api:executeWebhook(webhook[1], webhook[2], tbl)
 end
@@ -28,12 +29,12 @@ local evts = {
 			coroutine.wrap(function()
 				local msg = data.msg.txt
 				if not msg then return end
-
 				msg = util.cleanMassPings(msg)
 
 				if msg:match("@%w+") then
 					for mention in msg:gmatch("@(%w+)") do
 						local uid = findDiscordUserID(mention)
+
 						if uid then
 							msg = msg:gsub("@" .. mention, "<@" .. uid .. ">")
 						end
@@ -52,89 +53,99 @@ local evts = {
 	end,
 	disconnect = function(serverID, data)
 		coroutine.wrap(function()
-            config.channel:send({
-                embed = {
-                    author = {
-                        icon_url = data.disconnect.avatar or "http://i.imgur.com/ovW4MBM.png",
-                        name = data.disconnect.nickname .. " has left the server.",
-                        url = "http://steamcommunity.com/profiles/" .. data.disconnect.steamid
-                    },
-                    fields = data.disconnect.reason ~= "" and {
-                        [1] = {
-                            name = "Reason:",
-                            value = data.disconnect.reason
-                        }
-                    },
-                    footer = {
-                        text = "Server " .. serverID
-                    },
-                    color = 0xB54343
-                }
-            })
+			config.channel:send({
+				embed = {
+					author = {
+						icon_url = data.disconnect.avatar or "http://i.imgur.com/ovW4MBM.png",
+						name = data.disconnect.nickname .. " has left the server.",
+						url = "http://steamcommunity.com/profiles/" .. data.disconnect.steamid
+					},
+					fields = data.disconnect.reason ~= "" and {
+						[1] = {
+							name = "Reason:",
+							value = data.disconnect.reason
+						}
+					},
+					footer = {
+						text = "Server " .. serverID
+					},
+					color = 0xB54343
+				}
+			})
 		end)()
 	end,
 	spawn = function(serverID, data)
 		coroutine.wrap(function()
-            config.channel:send({
-                embed = {
-                    author = {
-                        icon_url = data.spawn.avatar or "http://i.imgur.com/ovW4MBM.png",
-                        name = data.spawn.nickname .. " has spawned.",
-                        url = "http://steamcommunity.com/profiles/" .. data.spawn.steamid
-                    },
-                    footer = {
-                        text = "Server " .. serverID
-                    },
-                    color = 0x4BB543
-                }
-            })
+			config.channel:send({
+				embed = {
+					author = {
+						icon_url = data.spawn.avatar or "http://i.imgur.com/ovW4MBM.png",
+						name = data.spawn.nickname .. " has spawned.",
+						url = "http://steamcommunity.com/profiles/" .. data.spawn.steamid
+					},
+					footer = {
+						text = "Server " .. serverID
+					},
+					color = 0x4BB543
+				}
+			})
 		end)()
 	end,
 	shutdown = function(serverID, data)
-		_G.status[serverID] = { players = {}, title = "Meta Construct " .. serverID, map = "gm_unknown" }
-        coroutine.wrap(function()
-            config.channel:send({
-                embed = {
-                    title = "Server " .. serverID .. " shutting down...",
-                    description = "Resetting status...",
-                    footer = {
-                            text = "Server " .. serverID
-                    },
-                    color = 0x0275d8
-                }
-            })
-        end)()
+		status[serverID] = {
+			players = {},
+			title = "Meta Construct " .. serverID,
+			map = "gm_unknown"
+		}
+
+		coroutine.wrap(function()
+			config.channel:send({
+				embed = {
+					title = "Server " .. serverID .. " shutting down...",
+					description = "Resetting status...",
+					footer = {
+						text = "Server " .. serverID
+					},
+					color = 0x0275d8
+				}
+			})
+		end)()
 	end,
 	notify = function(serverID, data)
 		if not data.notify.text then return end
-        coroutine.wrap(function()
-            config.channel:send({
-                embed = {
-                    title = data.notify.title or "",
-                    description = data.notify.text,
-                    footer = {
-                        text = "Server " .. serverID
-                    },
-                    color = data.notify.color or 0xffff00
-                }
-            })
-        end)()
+
+		coroutine.wrap(function()
+			config.channel:send({
+				embed = {
+					title = data.notify.title or "",
+					description = data.notify.text,
+					footer = {
+						text = "Server " .. serverID
+					},
+					color = data.notify.color or 0xffff00
+				}
+			})
+		end)()
 	end,
 	webhook = function(serverID, data)
 		if type(data.webhook) ~= "table" or next(data.webhook) == nil then return end
+		local wh = data.webhook
 
-        local wh = data.webhook
-        if wh.content or wh.embeds then
-            wh.content = wh.content and util.cleanMassPings(wh.content)
-            coroutine.wrap(function()
-                local ok, why = execWebhook(wh)
-                if not ok then config.channel:send(errorEmbed(why)) end
-            end)()
-        else
-            coroutine.wrap(function()
-                config.channel:send(errorEmbed("Received invalid embed?") )
-            end)()
-        end
+		if wh.content or wh.embeds then
+			wh.content = wh.content and util.cleanMassPings(wh.content)
+
+			coroutine.wrap(function()
+				local ok, why = execWebhook(wh)
+
+				if not ok then
+					config.channel:send(errorEmbed(why))
+				end
+			end)()
+		else
+			coroutine.wrap(function()
+				config.channel:send(errorEmbed("Received invalid embed?"))
+			end)()
+		end
 	end
 }
 
@@ -144,6 +155,7 @@ local function handleWS(data, write)
 	if type(data) == "string" then
 		data = json.parse(data) or {}
 	end
+
 	local server = "#" .. (data.server or "-1")
 
 	for name in next, data do
@@ -154,70 +166,79 @@ local function handleWS(data, write)
 end
 
 require("weblit-websocket")
-local app = require("weblit-app")
-	.bind({ host = "0.0.0.0", port = 20122 })
 
-	.use(require("weblit-logger"))
-	.use(require("weblit-auto-headers"))
+local app = require("weblit-app").bind({
+	host = "0.0.0.0",
+	port = 20122
+}).use(require("weblit-logger")).use(require("weblit-auto-headers")).websocket({
+	path = "/v2/socket"
+}, function(req, read, write)
+	loggedprint("New client")
+	loggedprint("Checking IP...")
+	local here = false
 
-	.websocket({ path = "/v2/socket" },	function(req, read, write)
-		loggedprint("New client")
-		loggedprint("Checking IP...")
+	for _, data in pairs(config.gameservers) do
+		local ip = req.socket:getpeername().ip
 
-		local here = false
-		for _, data in pairs(config.gameservers) do
-			local ip = req.socket:getpeername().ip
-			if ip == data.ip or ip == "::1" or ip == "::" or ip == "127.0.0.1" then
-				here = true
-			end
+		if ip == data.ip or ip == "::1" or ip == "::" or ip == "127.0.0.1" then
+			here = true
 		end
+	end
 
-		if not here then
-			loggedprint("Unknown IP: ", req.socket:getpeername().ip)
-			return write()
-		end
+	if not here then
+		loggedprint("Unknown IP: ", req.socket:getpeername().ip)
 
-		for message in read do
-			message.mask = nil
-
-            local ok, why = pcall(handleWS, message.payload, write)
-			if not ok then loggedprint(why) end
-
-			write(message)
-		end
-
-		loggedprint("Client left")
 		return write()
-	end)
+	end
 
-    .route({ method = "GET", path = "/discord/guild/emojis" }, function(req, res, go)
-        local emojis = {}
-        for emoji in config.guild.emojis:iter() do
-            emojis[tostring(emoji.id)] = {
-                createdAt = emoji.createdAt,
-                id = tostring(emoji.id),
-                timestamp = emoji.timestamp,
-                animated = emoji.animated,
-                mentionString = emoji.mentionString,
-                name = emoji.name,
-                url = emoji.url
-            }
-        end
+	for message in read do
+		message.mask = nil
+		local ok, why = pcall(handleWS, message.payload, write)
 
-        res.body = json.stringify(emojis)
-        res.code = 200
-        res.headers["Content-Type"] = "application/json"
-    end)
+		if not ok then
+			loggedprint(why)
+		end
+
+		write(message)
+	end
+
+	loggedprint("Client left")
+
+	return write()
+end).route({
+	method = "GET",
+	path = "/discord/guild/emojis"
+}, function(req, res, go)
+	local emojis = {}
+
+	for emoji in config.guild.emojis:iter() do
+		emojis[tostring(emoji.id)] = {
+			createdAt = emoji.createdAt,
+			id = tostring(emoji.id),
+			timestamp = emoji.timestamp,
+			animated = emoji.animated,
+			mentionString = emoji.mentionString,
+			name = emoji.name,
+			url = emoji.url
+		}
+	end
+
+	res.body = json.stringify(emojis)
+	res.code = 200
+	res.headers["Content-Type"] = "application/json"
+end)
 
 app.serverStatus = status
-
 local timer = require("timer")
+
 timer.setInterval(10000, function()
 	if client and status then
 		local str = ""
+
 		for id, data in next, status do
 			str = str .. (data.players and #data.players or "0") .. " players on " .. id .. " | "
 		end
+
 		str = str .. "!status"
 
 		coroutine.wrap(function()
