4c301542c446f5bcd9a466b9f112d44ea946aef7
Python1320 <python1320+gh@gmail.com>
2019-11-10 19:00:05 +0200

    Fix discord everyone spamming

--- helpers/util.lua
+++ helpers/util.lua
@@ -43,24 +43,33 @@ end
 
 -- the above is a fucking unused (?) mess
 
-_G.util = {} -- stuff used throughout multiple files
 
-
+_G.util = _G.util or {} -- stuff used throughout multiple files
 
 function util.cleanMassPings(str)
-    local ok
-    for i=1,32 do
-    	local n=0
-    	str,n1 = str:gsub("%\xE2%\x80%\xAE","") -- escape RTL chars that discord removes: https://github.com/Eufranio/MagiBridge/blob/6a946b0b32347b107b57fa947410d772104003ff/src/main/java/com/magitechserver/magibridge/discord/DiscordMessageBuilder.java#L32
-    	str,n2 = str:gsub("@+([Ee][Vv][Ee][Rr][Yy][Oo][Nn][Ee])", "%1")
-    	str,n3 = str:gsub("@+([Hh][Ee][Rr][Ee])", "%1")
-    	n=n1+n2+n3
-    	if n==0 then 
-    		ok=true
-    		break
-    	end
-    end
-    if not ok then return (str:gsub("[^a-zA-Z0-9]","")) end
-    return str
+	local ok, n1, n2, n3
+
+	-- Strip bad combinations until they no longer exist or until we have iterated too many times
+	for i = 1, 32 do
+		str, n1 = str:gsub("%\xE2%\x80%\xAE", "") -- escape RTL chars that discord removes: https://github.com/Eufranio/MagiBridge/blob/6a946b0b32347b107b57fa947410d772104003ff/src/main/java/com/magitechserver/magibridge/discord/DiscordMessageBuilder.java#L32
+		str, n2 = str:gsub("@+([Ee][Vv][Ee][Rr][Yy][Oo][Nn][Ee])", "%1")
+		str, n3 = str:gsub("@+([Hh][Ee][Rr][Ee])", "%1")
+
+		if n1 + n2 + n3 == 0 then
+			ok = true
+			break
+		end
+	end
+
+	if not ok then return (str:gsub("[^a-zA-Z0-9]", "")) end
+
+	return str
 end
 
+-- tests used
+
+--local a="@‮everyone U+202E RIGHT-TO-LEFT OVERRIDE"
+--local b="@\xE2\xE2\x80\xAE\x80\xAEeveryone"
+--
+--asd1=util.cleanMassPings(a)
+--asd2=util.cleanMassPings(b)
