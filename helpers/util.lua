f3372362a4438bc4795c0efbeb95bfc40f796aae
Cutler <dsecutler@gmail.com>
2019-12-11 13:04:11 -0500

    minor fix

--- helpers/util.lua
+++ helpers/util.lua
@@ -62,7 +62,7 @@ function util.cleanMassPings(str)
 	end
 
 	if not ok then return (str:gsub("[^a-zA-Z0-9]", "")) end
-	str:gsub("@", "\xE2\x80\x8B@\xE2\x80\x8B")
+	str = str:gsub("@", "\xE2\x80\x8B@\xE2\x80\x8B")
 	
 	return str
 end
